d36ebf56a3a8f774102d6d47d3e6f3a08fb1ce4b
Narnty <vladikslav17@gmail.com>
2021-05-21 19:12:48 +0500

    update Lohotron
    no -Credits and added LootBox

--- AntiClownBot/Models/Lohotron/Lohotron.cs
+++ AntiClownBot/Models/Lohotron/Lohotron.cs
@@ -14,8 +14,6 @@ namespace AntiClownBot.Models.Lohotron
         private static readonly ILohotronPrize[] AllPrizes =
         {
             new DefaultLohotronPrize(),
-            new CreditsLohotronPrize(-100),
-            new CreditsLohotronPrize(-200),
             new CreditsLohotronPrize(100),
             new CreditsLohotronPrize(200),
             new ItemLohotronPrize(new CatWife()),
@@ -23,22 +21,22 @@ namespace AntiClownBot.Models.Lohotron
             new ItemLohotronPrize(new Gigabyte()),
             new ItemLohotronPrize(new RiceBowl()),
             new ItemLohotronPrize(new CommunismPoster()),
-            new ItemLohotronPrize(new JadeRod())
+            new ItemLohotronPrize(new JadeRod()),
+            new ItemLohotronPrize(new LootBox())
         };
 
         private static readonly Wheel Wheel =
             new(
-                new(32, AllPrizes[0]), //Nothing
-                new(16, AllPrizes[1]), //-100 Credits
-                new(12, AllPrizes[2]), //-200 Credits
-                new(16, AllPrizes[3]), //+100 Credits
-                new(12, AllPrizes[4]), //+200 Credits
-                new(1, AllPrizes[5]), //CatWife
-                new(1, AllPrizes[6]), //DogWife
-                new(1, AllPrizes[7]), //Gigabyte
-                new(1, AllPrizes[8]), //RiceBowl
-                new(4, AllPrizes[9]), //CommunismPoster
-                new(4, AllPrizes[10]) //JadeRod
+                new(40, AllPrizes[0]), //Nothing
+                new(30, AllPrizes[1]), //+100 Credits
+                new(14, AllPrizes[2]), //+200 Credits
+                new(1, AllPrizes[3]), //CatWife
+                new(1, AllPrizes[4]), //DogWife
+                new(1, AllPrizes[5]), //Gigabyte
+                new(1, AllPrizes[6]), //RiceBowl
+                new(3, AllPrizes[7]), //CommunismPoster
+                new(3, AllPrizes[8]), //JadeRod
+                new (6, AllPrizes[9]) //Lootbox
             );
 
         public ILohotronPrize Play()
