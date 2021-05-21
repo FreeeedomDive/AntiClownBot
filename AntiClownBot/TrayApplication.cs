517acf98ee415885118d040d31afd66c21fa01fe
Igor Chernyshev <sonic96.ic@gmail.com>
2021-05-21 18:04:06 +0500

    dot net 5 migration

--- AntiClownBot/TrayApplication.cs
+++ AntiClownBot/TrayApplication.cs
@@ -7,7 +7,6 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 using AntiClownBot.Commands;
 using AntiClownBot.Commands.SocialRatingCommands;
 using AntiClownBot.Events;
