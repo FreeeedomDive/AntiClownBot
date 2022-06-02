0f4218d922b5fd13614ea670f840f1a9c4fad5a8
Igor Chernyshev <sonic96.ic@gmail.com>
2022-06-02 23:22:36 +0500

    Update TransfusionEvent.cs

--- AntiClownDiscordBot/Events/TransfusionEvent/TransfusionEvent.cs
+++ AntiClownDiscordBot/Events/TransfusionEvent/TransfusionEvent.cs
@@ -1,4 +1,5 @@
 ﻿using AntiClownBot.Helpers;
+using DSharpPlus.Entities;
 
 namespace AntiClownBot.Events.TransfusionEvent
 {
@@ -16,8 +17,8 @@ namespace AntiClownBot.Events.TransfusionEvent
                 exchangeUser = ApiWrapper.Wrappers.UsersApi.GetAllUsers().Users.SelectRandomItem();
             }
 
-            var richestMember = Configuration.GetServerMember(theRichestUser);
-            var exchangeMember = Configuration.GetServerMember(exchangeUser);
+            var richestMember = GetMember(theRichestUser);
+            var exchangeMember = GetMember(exchangeUser);
             
             await Utility.SendMessageToBotChannel("Я решил выделить немного кредитов рандомному челу, " +
                                  "но свой бюджет я тратить не буду, возьму из кармана самого богатого " +
@@ -26,6 +27,24 @@ namespace AntiClownBot.Events.TransfusionEvent
 
             Config.ChangeBalance(theRichestUser, -exchange, "Эвент перекачки");
             Config.ChangeBalance(exchangeUser, exchange, "Эвент перекачки");
+        }
+
+        private static DiscordMember GetMember(ulong id)
+        {
+            DiscordMember member = null;
+            while (member == null)
+            {
+                try
+                {
+                    member = Configuration.GetServerMember(id);
+                }
+                catch
+                {
+                    // ignored
+                }
+            }
+
+            return member;
         }
 
         protected override string BackStory()
