df204dce5b1cdbf2c01dcb2260ed5fb289cc45fa
Igor Chernyshev <sonic96.ic@gmail.com>
2022-06-06 23:48:57 +0500

    упси
    поднял билд

--- AntiClownApi.Client.Dto/Constants/NumericConstants.cs
+++ AntiClownApi.Client.Dto/Constants/NumericConstants.cs
@@ -2,9 +2,12 @@
 {
     public static class NumericConstants
     {
+        // 2% шанс срабатывания нефритового стержня для увеличения кулдауна
+        public const int CooldownIncreaseChanceByOneJade = 2;
+
         // стартовое значение возможного получения рейтинга с подношения
         public const int MinTributeValue = -40;
-        
+
         // конечное значение возможного получения рейтинга с подношения
         public const int MaxTributeValue = 100;
     }
