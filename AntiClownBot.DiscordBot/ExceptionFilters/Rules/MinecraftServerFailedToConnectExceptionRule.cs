e9dd17303fb915781759e71b2ccb8bc5693d9799
Igor Chernyshev <sonic96.ic@gmail.com>
2022-10-08 17:26:20 +0500

    или так

--- AntiClownBot.DiscordBot/ExceptionFilters/Rules/MinecraftServerFailedToConnectExceptionRule.cs
+++ AntiClownBot.DiscordBot/ExceptionFilters/Rules/MinecraftServerFailedToConnectExceptionRule.cs
@@ -1,13 +1,9 @@
-﻿using System.Net.Sockets;
-using Newtonsoft.Json;
-
-namespace AntiClownDiscordBotVersion2.ExceptionFilters.Rules;
+﻿namespace AntiClownDiscordBotVersion2.ExceptionFilters.Rules;
 
 public class MinecraftServerFailedToConnectExceptionRule : IExceptionFilterRule
 {
     public bool Filter(Exception exception)
     {
-        return exception is SocketException
-               && (exception.StackTrace?.Contains("TcpClient.CompleteConnectAsync") ?? false);
+        return exception.StackTrace?.Contains("TcpClient.CompleteConnectAsync") ?? false;
     }
 }
