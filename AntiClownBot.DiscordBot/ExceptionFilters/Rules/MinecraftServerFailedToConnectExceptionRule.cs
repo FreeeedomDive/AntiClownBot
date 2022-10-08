3e30d2f45a8d7eca2a24aa1d6a3f8f186f2b75af
Igor Chernyshev <sonic96.ic@gmail.com>
2022-10-08 17:20:38 +0500

    indent

--- AntiClownBot.DiscordBot/ExceptionFilters/Rules/MinecraftServerFailedToConnectExceptionRule.cs
+++ AntiClownBot.DiscordBot/ExceptionFilters/Rules/MinecraftServerFailedToConnectExceptionRule.cs
@@ -7,7 +7,7 @@ public class MinecraftServerFailedToConnectExceptionRule : IExceptionFilterRule
 {
     public bool Filter(Exception exception)
     {
-        Console.WriteLine(JsonConvert.SerializeObject(exception));
+        Console.WriteLine(JsonConvert.SerializeObject(exception, Formatting.Indented));
         return exception is SocketException
                && exception.Message == "Подключение не установлено, т.к. конечный компьютер отверг запрос на подключение";
     }
