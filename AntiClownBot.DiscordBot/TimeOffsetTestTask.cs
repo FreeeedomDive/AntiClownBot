1d181c1c868f7d4325070ca8944502e172185250
Igor Chernyshev <sonic96.ic@gmail.com>
2023-02-16 21:48:40 +0500

    TimeOffsetTestTask

---
+++ AntiClownBot.DiscordBot/TimeOffsetTestTask.cs
@@ -0,0 +1,27 @@
+﻿using TelemetryApp.Api.Client.Log;
+
+namespace AntiClownDiscordBotVersion2;
+
+public class TimeOffsetTestTask
+{
+    private readonly ILoggerClient log;
+
+    public TimeOffsetTestTask(ILoggerClient log)
+    {
+        this.log = log;
+    }
+
+    public void Start()
+    {
+        Task.Run(StartAsync);
+    }
+
+    private async Task StartAsync()
+    {
+        while (true)
+        {
+            await log.DebugAsync("Current time is {time}, lets sleep for exactly 2 hours Bedge", DateTime.Now);
+            await Task.Delay(2 * 60 * 60 * 1000);
+        }
+    }
+}
