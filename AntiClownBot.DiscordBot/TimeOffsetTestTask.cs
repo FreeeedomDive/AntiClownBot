41e4c5532de684f429f108275fca7c442bb753f7
Igor Chernyshev <sonic96.ic@gmail.com>
2023-02-16 22:02:10 +0500

    better time formatting for log

--- AntiClownBot.DiscordBot/TimeOffsetTestTask.cs
+++ AntiClownBot.DiscordBot/TimeOffsetTestTask.cs
@@ -20,7 +20,11 @@ public class TimeOffsetTestTask
     {
         while (true)
         {
-            await log.DebugAsync("Current time is {time}, lets sleep for exactly 2 hours Bedge", DateTime.Now);
+            var now = DateTime.Now;
+            await log.DebugAsync(
+                "Current time is {time}, lets sleep for exactly 2 hours Bedge", 
+                $"{now.Day}.{now.Month}.{now.Year} {now.Hour}:{now.Minute}:{now.Second}.{now.Millisecond}"
+                );
             await Task.Delay(2 * 60 * 60 * 1000);
         }
     }
