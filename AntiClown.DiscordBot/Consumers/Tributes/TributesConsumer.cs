fffde6a3bf3e542321b1f764f1e3832cb13e850e
Igor Chernyshev <sonic96.ic@gmail.com>
2023-07-15 01:58:10 +0500

    basic consumer for auto tributes

---
+++ AntiClown.DiscordBot/Consumers/Tributes/TributesConsumer.cs
@@ -0,0 +1,31 @@
+﻿using AntiClown.Messages.Dto.Tributes;
+using MassTransit;
+using Newtonsoft.Json;
+
+namespace AntiClown.DiscordBot.Consumers.Tributes;
+
+public class TributesConsumer : IConsumer<TributeMessageDto>
+{
+    public TributesConsumer(
+        ILogger<TributesConsumer> logger
+    )
+    {
+        this.logger = logger;
+    }
+
+    public Task Consume(ConsumeContext<TributeMessageDto> context)
+    {
+        var tribute = context.Message;
+        logger.LogInformation(
+            "{ConsumerName} received tribute by {UserId}\n{Tribute}",
+            ConsumerName,
+            tribute.UserId,
+            JsonConvert.SerializeObject(tribute)
+        );
+
+        return Task.CompletedTask;
+    }
+
+    private static string ConsumerName => nameof(TributesConsumer);
+    private readonly ILogger<TributesConsumer> logger;
+}
