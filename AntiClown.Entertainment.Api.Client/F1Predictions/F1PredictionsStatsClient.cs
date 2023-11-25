9d20e6d627b35ccc2243282ded06f41dd52eb7fe
Igor Chernyshev <sonic96.ic@gmail.com>
2023-11-25 16:47:40 +0500

    add client

---
+++ AntiClown.Entertainment.Api.Client/F1Predictions/F1PredictionsStatsClient.cs
@@ -0,0 +1,29 @@
+﻿using AntiClown.Entertainment.Api.Client.Extensions;
+using AntiClown.Entertainment.Api.Dto.F1Predictions.Statistics;
+using RestSharp;
+
+namespace AntiClown.Entertainment.Api.Client.F1Predictions;
+
+public class F1PredictionsStatsClient : IF1PredictionsStatsClient
+{
+    public F1PredictionsStatsClient(RestClient restClient)
+    {
+        this.restClient = restClient;
+    }
+
+    public async Task<MostPickedDriversByUsersStatsDto> GetMostPickedDriversByUsersAsync()
+    {
+        var request = new RestRequest("f1Predictions/stats/mostPickedDriversByUsers");
+        var response = await restClient.ExecuteGetAsync(request);
+        return response.TryDeserialize<MostPickedDriversByUsersStatsDto>();
+    }
+
+    public async Task<MostProfitableDriversStatsDto> GetMostProfitableDriversAsync()
+    {
+        var request = new RestRequest("f1Predictions/stats/mostProfitableDrivers");
+        var response = await restClient.ExecuteGetAsync(request);
+        return response.TryDeserialize<MostProfitableDriversStatsDto>();
+    }
+
+    private readonly RestClient restClient;
+}
---
+++ AntiClown.Entertainment.Api.Client/F1Predictions/IF1PredictionsStatsClient.cs
@@ -0,0 +1,9 @@
+﻿using AntiClown.Entertainment.Api.Dto.F1Predictions.Statistics;
+
+namespace AntiClown.Entertainment.Api.Client.F1Predictions;
+
+public interface IF1PredictionsStatsClient
+{
+    Task<MostPickedDriversByUsersStatsDto> GetMostPickedDriversByUsersAsync();
+    Task<MostProfitableDriversStatsDto> GetMostProfitableDriversAsync();
+}
