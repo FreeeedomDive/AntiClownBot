cb0af1410348cd5c32efeb4fb2edc354424e57e9
Igor Chernyshev <sonic96.ic@gmail.com>
2023-12-31 13:39:54 +0500

    add front to local start suites

--- LocalStartSuites/startAll.bat
+++ LocalStartSuites/startAll.bat
@@ -9,5 +9,6 @@ kubectl apply -f anticlown-api-deployment.yaml
 kubectl apply -f anticlown-entertainment-api-deployment.yaml
 kubectl apply -f anticlown-events-daemon-deployment.yaml
 kubectl apply -f anticlown-discord-bot-deployment.yaml
+kubectl apply -f anticlown-web-front-deployment.yaml
 kubectl apply -f anticlown-nginx-deployment.yaml
 pause
