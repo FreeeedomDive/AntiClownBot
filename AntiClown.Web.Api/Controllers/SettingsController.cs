345d98d1c9cf7fada380d9f6a659d951460554e4
Igor Chernyshev <sonic96.ic@gmail.com>
2024-05-04 15:26:20 +0500

    add webapi controller

---
+++ AntiClown.Web.Api/Controllers/SettingsController.cs
@@ -0,0 +1,29 @@
+﻿using AntiClown.Data.Api.Client;
+using AntiClown.Data.Api.Dto.Settings;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AntiClown.Web.Api.Controllers;
+
+[Route("webApi/settings")]
+public class SettingsController : Controller
+{
+    public SettingsController(IAntiClownDataApiClient antiClownDataApiClient)
+    {
+        this.antiClownDataApiClient = antiClownDataApiClient;
+    }
+
+    [HttpGet("categories/{category}")]
+    public async Task<ActionResult<SettingDto[]>> FindAsync([FromRoute] string category)
+    {
+        return await antiClownDataApiClient.Settings.FindAsync(category);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CreateOrUpdateAsync([FromBody] SettingDto settingDto)
+    {
+        await antiClownDataApiClient.Settings.CreateOrUpdateAsync(settingDto.Category, settingDto.Name, settingDto.Value);
+        return NoContent();
+    }
+
+    private readonly IAntiClownDataApiClient antiClownDataApiClient;
+}
