cf4b1079fa5399820d3ff1a7ffd549452065c836
Igor Chernyshev <sonic96.ic@gmail.com>
2024-06-20 23:35:30 +0500

    update generator

--- ClientGenerator/Program.cs
+++ ClientGenerator/Program.cs
@@ -5,13 +5,21 @@ using AntiClown.Entertainment.Api.Controllers.Parties;
 using Xdd.HttpHelpers.HttpClientGenerator;
 
 // Api
-ApiClientGenerator.Generate<UsersController>(Path.Join("..", "..", "..", "..", "AntiClown.Api.Client"));
+ApiClientGenerator.Generate<UsersController>(
+    options => options.ProjectPath = Path.Join("..", "..", "..", "..", "AntiClown.Api.Client")
+);
 
 // EntertainmentApi
-ApiClientGenerator.Generate<PartiesController>(Path.Join("..", "..", "..", "..", "AntiClown.Entertainment.Api.Client"));
+ApiClientGenerator.Generate<PartiesController>(
+    options => options.ProjectPath = Path.Join("..", "..", "..", "..", "AntiClown.Entertainment.Api.Client")
+);
 
 // DataApi
-ApiClientGenerator.Generate<TokensController>(Path.Join("..", "..", "..", "..", "AntiClown.Data.Api.Client"));
+ApiClientGenerator.Generate<TokensController>(
+    options => options.ProjectPath = Path.Join("..", "..", "..", "..", "AntiClown.Data.Api.Client")
+);
 
 // DiscordApi
-ApiClientGenerator.Generate<DiscordMembersController>(Path.Join("..", "..", "..", "..", "AntiClown.DiscordBot.Client"));
+ApiClientGenerator.Generate<DiscordMembersController>(
+    options => options.ProjectPath = Path.Join("..", "..", "..", "..", "AntiClown.DiscordBot.Client")
+);
