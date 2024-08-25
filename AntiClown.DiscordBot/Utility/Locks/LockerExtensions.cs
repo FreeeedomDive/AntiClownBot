76e8dc26be5fcb34fef9ac6a7435d638113f3987
Igor Chernyshev <sonic96.ic@gmail.com>
2024-08-25 16:04:00 +0500

    fix locker not releasing lock due to exception

--- AntiClown.DiscordBot/Utility/Locks/LockerExtensions.cs
+++ AntiClown.DiscordBot/Utility/Locks/LockerExtensions.cs
@@ -4,16 +4,27 @@ public static class LockerExtensions
 {
     public static async Task<T> ReadInLockAsync<T>(this ILocker locker, string lockKey, Func<Task<T>> func)
     {
-        await locker.AcquireAsync(lockKey);
-        var result = await func();
-        await locker.ReleaseAsync(lockKey);
-        return result;
+        try
+        {
+            await locker.AcquireAsync(lockKey);
+            return await func();
+        }
+        finally
+        {
+            await locker.ReleaseAsync(lockKey);
+        }
     }
 
     public static async Task DoInLockAsync(this ILocker locker, string lockKey, Func<Task> action)
     {
-        await locker.AcquireAsync(lockKey);
-        await action();
-        await locker.ReleaseAsync(lockKey);
+        try
+        {
+            await locker.AcquireAsync(lockKey);
+            await action();
+        }
+        finally
+        {
+            await locker.ReleaseAsync(lockKey);
+        }
     }
 }
