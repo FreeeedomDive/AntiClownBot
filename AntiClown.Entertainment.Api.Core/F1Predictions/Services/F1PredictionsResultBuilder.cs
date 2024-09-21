907c0eaa9853919974af1d6cfad50085196e8110
Igor Chernyshev <sonic96.ic@gmail.com>
2024-09-21 17:13:07 +0500

    magnussen again

--- AntiClown.Entertainment.Api.Core/F1Predictions/Services/F1PredictionsResultBuilder.cs
+++ AntiClown.Entertainment.Api.Core/F1Predictions/Services/F1PredictionsResultBuilder.cs
@@ -30,7 +30,7 @@ public static class F1PredictionsResultBuilder
             SelectHighestTeamMate(driverToPosition, F1Driver.Piastri, F1Driver.Norris),
             SelectHighestTeamMate(driverToPosition, F1Driver.Bottas, F1Driver.Zhou),
             SelectHighestTeamMate(driverToPosition, F1Driver.Stroll, F1Driver.Alonso),
-            SelectHighestTeamMate(driverToPosition, F1Driver.Bearman, F1Driver.Hulkenberg),
+            SelectHighestTeamMate(driverToPosition, F1Driver.Magnussen, F1Driver.Hulkenberg),
             SelectHighestTeamMate(driverToPosition, F1Driver.Ricciardo, F1Driver.Tsunoda),
             SelectHighestTeamMate(driverToPosition, F1Driver.Albon, F1Driver.Colapinto),
         };
