7d4e4f6c66eaac21b137755949240daba7db4443
Igor Chernyshev <sonic96.ic@gmail.com>
2025-03-09 15:35:08 +0500

    generate migrations

--- AntiClown.Entertainment.Api.Core/Migrations/20250309102710_F1Bingo.Designer.cs
+++ AntiClown.Entertainment.Api.Core/Migrations/20250309102710_F1Bingo.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AntiClown.Entertainment.Api.Core.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace AntiClown.Entertainment.Api.Core.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20250309102710_F1Bingo")]
+    partial class F1Bingo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AntiClown.Entertainment.Api.Core/Migrations/20250309102710_F1Bingo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AntiClown.Entertainment.Api.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class F1Bingo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "F1BingoBoards",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Season = table.Column<int>(type: "integer", nullable: false),
+                    Cards = table.Column<Guid[]>(type: "uuid[]", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_F1BingoBoards", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "F1BingoCards",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Season = table.Column<int>(type: "integer", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Explanation = table.Column<string>(type: "text", nullable: true),
+                    Probability = table.Column<string>(type: "text", nullable: false),
+                    TotalRepeats = table.Column<int>(type: "integer", nullable: false),
+                    CompletedRepeats = table.Column<int>(type: "integer", nullable: false),
+                    IsCompleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_F1BingoCards", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_F1BingoBoards_UserId_Season",
+                table: "F1BingoBoards",
+                columns: new[] { "UserId", "Season" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_F1BingoCards_Season",
+                table: "F1BingoCards",
+                column: "Season");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "F1BingoBoards");
+
+            migrationBuilder.DropTable(
+                name: "F1BingoCards");
+        }
+    }
+}
