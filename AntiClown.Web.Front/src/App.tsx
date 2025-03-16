dceff541e679b58d182a55448e797226beefc189
Igor Chernyshev <sonic96.ic@gmail.com>
2025-03-16 13:28:01 +0500

    add common url for predictions standings and redirect to correct page

--- AntiClown.Web.Front/src/App.tsx
+++ AntiClown.Web.Front/src/App.tsx
@@ -3,7 +3,8 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import UserMainPage from "./Routes/User/MainPage/UserMainPage";
 import LoginPage from "./Routes/Auth/LoginPage";
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
+import { useStore } from "./Stores";
 
 const darkTheme = createTheme({
   palette: {
@@ -16,8 +17,11 @@ const darkTheme = createTheme({
 
 function App() {
   useEffect(() => {
-    document.title = 'Clown City Web';
+    document.title = "Clown City Web";
   }, []);
+  const { authStore } = useStore();
+  const userId = authStore.loggedInUserId;
+
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
@@ -26,6 +30,7 @@ function App() {
           <Route path="/" element={<Navigate to={"/auth"} />} />
           <Route path="/auth" element={<LoginPage />} />
           <Route path="/user/:userId/*" element={<UserMainPage />} />
+          <Route path="/f1Predictions" element={<Navigate to={`/user/${userId}/f1Predictions/standings`} />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
