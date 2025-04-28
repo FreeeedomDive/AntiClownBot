79b154244e3fac3669252d6087dfa46cc7e6e89d
Igor Chernyshev <sonic96.ic@gmail.com>
2025-04-28 20:08:28 +0500

    generate migration

---
+++ AntiClown.Api.Core/Migrations/20250428150749_AddYandexIdColumnToUserTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AntiClown.Api.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddYandexIdColumnToUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "YandexId",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "YandexId",
+                table: "Users");
+        }
+    }
+}
