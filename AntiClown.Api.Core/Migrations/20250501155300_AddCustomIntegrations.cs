0e819d68e3c8e165b1e1220ae9126994db933b3f
Igor Chernyshev <sonic96.ic@gmail.com>
2025-05-01 20:55:35 +0500

    add migration

---
+++ AntiClown.Api.Core/Migrations/20250501155300_AddCustomIntegrations.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AntiClown.Api.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCustomIntegrations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Integrations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    IntegrationName = table.Column<string>(type: "text", nullable: false),
+                    IntegrationUserId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Integrations", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Integrations_IntegrationName_IntegrationUserId",
+                table: "Integrations",
+                columns: new[] { "IntegrationName", "IntegrationUserId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Integrations");
+        }
+    }
+}
