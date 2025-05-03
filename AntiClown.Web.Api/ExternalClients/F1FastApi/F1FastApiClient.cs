e75eafa80a8def6a93cc47ff53d5aca8710e8463
Igor Chernyshev <sonic96.ic@gmail.com>
2025-05-03 18:33:54 +0500

    log result

--- AntiClown.Web.Api/ExternalClients/F1FastApi/F1FastApiClient.cs
+++ AntiClown.Web.Api/ExternalClients/F1FastApi/F1FastApiClient.cs
@@ -3,14 +3,14 @@ using AntiClown.Web.Api.Options;
 using Microsoft.Extensions.Options;
 using RestSharp;
 using Xdd.HttpHelpers.Models.Extensions;
-using Xdd.HttpHelpers.Models.Requests;
 
 namespace AntiClown.Web.Api.ExternalClients.F1FastApi;
 
 public class F1FastApiClient : IF1FastApiClient
 {
-    public F1FastApiClient(IOptions<F1FastApiOptions> options)
+    public F1FastApiClient(IOptions<F1FastApiOptions> options, ILogger<F1FastApiClient> logger)
     {
+        this.logger = logger;
         var restClientOptions = new RestClientOptions
         {
             BaseUrl = new Uri(options.Value.ServiceUrl),
@@ -22,12 +22,15 @@ public class F1FastApiClient : IF1FastApiClient
 
     public async Task<F1PredictionRaceResultDto> GetF1PredictionRaceResult(Guid raceId, bool isSprint)
     {
-        var request = new RequestBuilder("", HttpRequestMethod.GET)
-                      .WithQueryParameter("raceId", raceId)
-                      .WithQueryParameter("raceType", isSprint ? "Sprint" : "Race")
-                      .Build();
-        return await restClient.MakeRequestAsync<F1PredictionRaceResultDto>(request);
+        var request = new RestRequest(string.Empty)
+                      .AddQueryParameter("raceId", raceId)
+                      .AddQueryParameter("raceType", isSprint ? "Sprint" : "Race");
+        var response = await restClient.ExecuteAsync(request);
+        logger.LogInformation("F1FastApi Response:\n{response}", response);
+        return response.TryDeserialize<F1PredictionRaceResultDto>();
     }
+
+    private readonly ILogger<F1FastApiClient> logger;
 
     private readonly RestClient restClient;
 }
