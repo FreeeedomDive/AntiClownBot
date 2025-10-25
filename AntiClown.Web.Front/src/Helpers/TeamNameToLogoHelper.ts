8b573caac0edfad53f1470cf93302e54bb288f53
Igor Chernyshev <sonic96.ic@gmail.com>
2025-10-26 08:34:03 +0900

    fix teams logos

--- AntiClown.Web.Front/src/Helpers/TeamNameToLogoHelper.ts
+++ AntiClown.Web.Front/src/Helpers/TeamNameToLogoHelper.ts
@@ -1,41 +1,41 @@
 export function teamNameToLogo(teamName: string): string {
-  if (teamName === "Alpine") {
+  if (teamName === "06 Alpine") {
     return "https://sportsmintmedia.com/wp-content/uploads/2022/02/Alpine-F1-team-announces-BWT-as-title-sponsor.jpg";
   }
 
-  if (teamName === "Aston Martin") {
+  if (teamName === "05 Aston Martin") {
     return "https://cdn-5.motorsport.com/images/mgl/68yGpmX0/s200/logo-aston-martin.webp";
   }
 
-  if (teamName === "Ferrari") {
+  if (teamName === "02 Ferrari") {
     return "https://upload.wikimedia.org/wikipedia/en/d/df/Scuderia_Ferrari_HP_logo_24.svg";
   }
 
-  if (teamName === "Haas") {
+  if (teamName === "07 Haas") {
     return "https://cdn-4.motorsport.com/images/mgl/0ZRgKzM0/s200/haas-logo-1.webp";
   }
 
-  if (teamName === "McLaren") {
+  if (teamName === "01 McLaren") {
     return "https://cdn-2.motorsport.com/images/mgl/YEQJgyLY/s200/mclaren-f1-team-logo-1.webp";
   }
 
-  if (teamName === "Mercedes") {
+  if (teamName === "04 Mercedes") {
     return "https://cdn-1.motorsport.com/images/mgl/0RrzmDo0/s200/mercedes-f1-logo-1.webp";
   }
 
-  if (teamName === "RB") {
+  if (teamName === "08 RB") {
     return "https://cdn-1.motorsport.com/images/mgl/Y998adGY/s200/visa-cash-app-rb-logo.webp";
   }
 
-  if (teamName === "Red Bull Racing") {
+  if (teamName === "03 Red Bull Racing") {
     return "https://cdn-3.motorsport.com/images/mgl/Y99JQRbY/s200/red-bull-racing-logo-1.webp";
   }
 
-  if (teamName === "Stake") {
+  if (teamName === "10 Stake") {
     return "https://cdn-2.motorsport.com/images/mgl/0L17d5W2/s200/logo-stakef1team-rgb-pos-1.webp";
   }
 
-  if (teamName === "Williams") {
+  if (teamName === "09 Williams") {
     return "https://cdn-7.motorsport.com/images/mgl/2y39NBy6/s200/williams-racing-logo-1.webp";
   }
 
