b1f828f58662a50ab33968fc08d9db68aaf77814
Igor Chernyshev <sonic96.ic@gmail.com>
2026-03-12 00:30:21 +0500

    add missing space

--- src/DiscordBot/AntiClown.DiscordBot/SlashCommands/F1Predictions/F1CommandModule.cs
+++ src/DiscordBot/AntiClown.DiscordBot/SlashCommands/F1Predictions/F1CommandModule.cs
@@ -80,7 +80,7 @@ public class F1CommandModule(
                 var userToMember = standings.Keys.ToDictionary(x => x, x => usersCache.GetMemberByApiIdAsync(x).GetAwaiter().GetResult());
                 var longestNameLength = userToMember.Values.Select(x => x.ServerOrUserName().Length).Max();
                 var stringBuilder = new StringBuilder($"Полная таблица: {webOptions.Value.FrontApplicationUrl}/f1Predictions/standings")
-                    .Append($"\nРегламент:{webOptions.Value.FrontApplicationUrl}/f1Predictions/rulebook")
+                    .Append($"\nРегламент: {webOptions.Value.FrontApplicationUrl}/f1Predictions/rulebook")
                     .Append("\n```\n");
                 const int maxPossiblePoints = F1PredictionsHelper.MaxPointsForTenthPlacePrediction;
                 var predictionsTable = standings
