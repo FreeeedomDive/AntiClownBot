f2f435c9fac81102e48d05c862fc8bd262c314c1
Igor Chernyshev <sonic96.ic@gmail.com>
2026-08-01 01:08:01 +0500

    add view for trophies

--- src/Web/AntiClown.Web.Front/src/Dto/F1Predictions/F1StandingsRowDto.ts
+++ src/Web/AntiClown.Web.Front/src/Dto/F1Predictions/F1StandingsRowDto.ts
@@ -1,7 +1,9 @@
 import { F1PredictionUserResultDto } from "./F1PredictionUserResultDto";
+import { F1PodiumDto } from "./F1PodiumDto";
 
 export interface F1StandingsRowDto {
   userId: string;
   totalPoints: number;
-  results: F1PredictionUserResultDto[];
+  results: (F1PredictionUserResultDto | null)[];
+  previousPodiums: F1PodiumDto[];
 }
